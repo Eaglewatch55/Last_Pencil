9c8253fba710daf9059b0534009dff336b00c563
Eaglewatch55 <orlandogarcia94@hotmail.com>
2023-02-16 12:36:51 -0600

    Satge 4/5

---
+++ Last Pencil/task/tests.py
@@ -0,0 +1,4 @@
+from test.tests import LastPencilTest
+
+if __name__ == '__main__':
+    LastPencilTest().run_tests()
---
+++ Topics/The string module/Capitalize 2.0/main.py
@@ -0,0 +1,7 @@
+import string
+
+line1 = "no one knows what it's like"
+line2 = "to be the bad man"
+
+print(line1.capitalize())
+print(string.capwords(line2))
